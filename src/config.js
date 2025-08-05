d32029a22cd742e438dd0557a70606869bade99c
jesuSando <correoepicgames@gmail.com>
2025-08-05 09:33:42 -0400

    arreglo url

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 // Variables globales configurables de tu app
 export const CONFIG = {
-    BASE_API_URL: 'http://localhost:4000/api',
+    BASE_API_URL: 'https://localhost:4000/api',
     PAGE_SIZE: 10,
 };
